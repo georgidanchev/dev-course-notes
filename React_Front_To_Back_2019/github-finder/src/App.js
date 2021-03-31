e4f3b2e11787a7768c8761491c8ac804d33bc686
w3althambition <georgicodes@outlook.com>
2021-03-31 22:09:15 +0100

    react project progress - section 3 completed

--- React_Front_To_Back_2019/github-finder/src/App.js
+++ React_Front_To_Back_2019/github-finder/src/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
-import Navbar from './Components/layout/Navbar';
-import Users from './Components/users/Users';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./Components/layout/Navbar";
+import Users from "./Components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data, loading: false});
+  }
+
   render() {
     return (
       <div className="app">
         <Navbar />
         <div className="container">
-          <Users/>
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
--- React_Front_To_Back_2019/github-finder/src/Components/users/Users.js
+++ React_Front_To_Back_2019/github-finder/src/Components/users/Users.js
@@ -1,45 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "2",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "3",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-    ],
-  };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
-  display: 'grid',
+  display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
-  gridGap: '1rem',
-}
+  gridGap: "1rem",
+};
 
 export default Users;
