a5a64d06d1c5a9b570e0ceb9b12485871da57fb4
w3althambition <georgicodes@outlook.com>
2021-04-23 18:00:23 +0100

    react course, section 8 - setup of front end, navbar, context/global state and router

---
+++ React_Front_To_Back_2019/contact-keeper-mern/client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p class="my-1">This is a full stack React app for keeping contacts</p>
+      <p class="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
