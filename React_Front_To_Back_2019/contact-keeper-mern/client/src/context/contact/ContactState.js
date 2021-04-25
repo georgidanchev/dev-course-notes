a33e440d1dd4ca6baf630175e6a8cc62c2271190
w3althambition <georgicodes@outlook.com>
2021-04-25 19:51:03 +0100

    react course section 9 react and express auth completed

--- React_Front_To_Back_2019/contact-keeper-mern/client/src/context/contact/ContactState.js
+++ React_Front_To_Back_2019/contact-keeper-mern/client/src/context/contact/ContactState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import { v4 as uuid } from "uuid";
 import ContactContext from "./contactContext";
-import contactReducer from "./ContactReducer";
+import contactReducer from "./contactReducer";
 import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from "../types";
 
 const ContactState = (props) => {
