7b0b337230ef9e1bacf76251aaeefa47bf928169
w3althambition <georgicodes@outlook.com>
2021-05-03 18:37:47 +0100

    react course section 11 components and UI completed

---
+++ React_Front_To_Back_2019/it-logger/src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = (props) => {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="indeterminate blue"></div>
+    </div>
+  );
+};
+
+export default Preloader;
---
+++ React_Front_To_Back_2019/it-logger/src/components/techs/TechListModal.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../layout/Preloader";
+import TechItem from "./TechItem";
+
+const TechListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    // eslint-disable-next-line
+  }, []);
+
+  const getTechs = async () => {
+    setLoading(true);
+    const res = await fetch("/techs");
+    const data = await res.json();
+    setTechs(data);
+    setLoading(false);
+  };
+
+  return (
+    <div id="tech-list-modal" className="modal">
+      <div className="modal-content">
+        <h4>Technician List</h4>
+        <ul className="collection">{!loading && techs !== null && techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechListModal;
