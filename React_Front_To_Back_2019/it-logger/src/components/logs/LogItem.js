03c49c9fdadf236e8b6b2690a3291ba17837d241
w3althambition <georgicodes@outlook.com>
2021-05-07 08:38:47 +0100

    react course - delete feature implemented

--- React_Front_To_Back_2019/it-logger/src/components/logs/LogItem.js
+++ React_Front_To_Back_2019/it-logger/src/components/logs/LogItem.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import Moment from "react-moment";
+import { deleteLog } from "../../actions/logActions";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: "Log Deleted" });
+  };
+
   return (
     <li className="collection-item">
       <div>
@@ -14,7 +22,7 @@ const LogItem = ({ log }) => {
           <span className="black-text">ID #{log.id}</span> last updated by
           <span className="black-text"> {log.tech}</span> on <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a href="#!" onClick={onDelete} className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -24,6 +32,7 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
 };
 
-export default LogItem;
+export default connect(null, { deleteLog })(LogItem);
--- React_Front_To_Back_2019/it-logger/src/reducers/logReducer.js
+++ React_Front_To_Back_2019/it-logger/src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "../actions/types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "../actions/types";
 
 const initialState = {
   logs: null,
@@ -25,6 +25,12 @@ export default (state = initialState, action) => {
       return {
         ...state,
         loading: true,
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
       };
     case LOGS_ERROR:
       console.error(action.payload);
