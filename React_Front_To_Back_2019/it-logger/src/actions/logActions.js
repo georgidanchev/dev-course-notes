8760dfa4c488f692d1ffe355487d952a4d7164d3
Georgi Danchev <georgicodes@outlook.com>
2021-05-08 12:00:34 +0100

    react course - search function implemented

--- React_Front_To_Back_2019/it-logger/src/actions/logActions.js
+++ React_Front_To_Back_2019/it-logger/src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS } from "./types";
 
 // Get logs from server
 export const getLogs = () => async (dispatch) => {
@@ -69,6 +69,24 @@ export const updateLog = (log) => async (dispatch) => {
 
     dispatch({
       type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Search server logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data,
     });
   } catch (err) {
--- React_Front_To_Back_2019/it-logger/src/reducers/logReducer.js
+++ React_Front_To_Back_2019/it-logger/src/reducers/logReducer.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, UPDATE_LOG, SET_CURRENT, CLEAR_CURRENT } from "../actions/types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SEARCH_LOGS,
+} from "../actions/types";
 
 const initialState = {
   logs: null,
@@ -21,7 +31,6 @@ export default (state = initialState, action) => {
         logs: [...state.logs, action.payload],
         loading: false,
       };
-
     case SET_LOADING:
       return {
         ...state,
@@ -37,6 +46,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         logs: state.logs.map((log) => (log.id === action.payload.id ? action.payload : log)),
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
       };
     case SET_CURRENT:
       return {
