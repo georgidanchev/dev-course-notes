f0304ccdfd391c96d110c93e8de947d174e363ac
Georgi Danchev <georgicodes@outlook.com>
2022-02-11 16:55:23 +0000

    3d effect background boxes completed

---
+++ 50_Projects_in_50_Days/40_3d_background_boxes/script.js
@@ -0,0 +1,21 @@
+const boxesContainer = document.getElementById("boxes")
+const btn = document.getElementById("btn")
+
+const createBoxes = () => {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div")
+      box.classList.add("box")
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      boxesContainer.appendChild(box)
+    }
+  }
+}
+
+btn.addEventListener("click", () => {
+  boxesContainer.classList.toggle("big")
+})
+
+boxesContainer.innerHTML = ""
+
+createBoxes()
