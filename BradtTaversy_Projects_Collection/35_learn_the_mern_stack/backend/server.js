c6792999ed62d2f8ad6c5cfc8f46a898a5752d61
Georgi Danchev <georgicodes@outlook.com>
2022-02-16 10:03:32 +0000

    mern goals controller and routes setup

--- BradtTaversy_Projects_Collection/35_learn_the_mern_stack/backend/server.js
+++ BradtTaversy_Projects_Collection/35_learn_the_mern_stack/backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+const express = require("express")
+const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
+const port = process.env.PORT || 5001
+
+const app = express()
+
+// JSON parser for express
+app.use(express.json())
+
+// Middleware to allow use of req.body data
+app.use(express.urlencoded({ extended: false }))
+
+// All paths
+app.use("/api/goals", require("./routes/goalRoutes"))
+
+// Overwrite the default error handler
+app.use(errorHandler)
+
+// Server starts listening
+app.listen(port, () => console.log(`Server started on port ${port}`))
