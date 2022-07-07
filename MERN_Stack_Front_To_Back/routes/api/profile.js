ef9989e8a0a60e333ab1d75abbf9e3a07c220663
Georgi Danchev <georgicodes@outlook.com>
2022-07-07 11:25:32 +0100

    mern app: add and delete profile experience

--- MERN_Stack_Front_To_Back/routes/api/profile.js
+++ MERN_Stack_Front_To_Back/routes/api/profile.js
@@ -148,4 +148,74 @@ router.delete("/", auth, async (req, res) => {
   }
 })
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { title, company, from, to, current, description } = req.body
+
+    const newExp = {
+      title,
+      company,
+      from,
+      to,
+      current,
+      description,
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.experience.unshift(newExp)
+      await profile.save()
+      res.json(profile)
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("Server Error")
+    }
+  }
+)
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // Getting the profile of the user
+    // and then getting the index
+    const removeIndex = profile.experience.filter(
+      (item) => item.id !== req.params.exp_id
+    )
+
+    // Removing the index
+    profile.experience.splice(removeIndex, 1)
+
+    // Saving the chances
+    await profile.save()
+
+    // Return the profile as response
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send("Server Error")
+  }
+})
+
 module.exports = router
