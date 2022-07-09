6b1feff84c4a710d897e8b9407c88a3659205ca9
Georgi Danchev <georgicodes@outlook.com>
2022-07-09 14:59:00 +0100

    mern app: redux store and alert reducer

--- MERN_Stack_Front_To_Back/client/src/App.js
+++ MERN_Stack_Front_To_Back/client/src/App.js
@@ -1,22 +1,28 @@
 import { Fragment } from "react"
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Landing } from "./components/layout/Landing"
 import { Navbar } from "./components/layout/Navbar"
 import { Register } from "./components/auth/Register"
 import { Login } from "./components/auth/Login"
 import "./App.css"
 
+// Redux
+import { Provider } from "react-redux"
+import store from "./store"
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 )
 
 export default App
---
+++ MERN_Stack_Front_To_Back/client/src/components/reducers/alert.js
@@ -0,0 +1,19 @@
+const { SET_ALERT, REMOVE_ALERT } = require("../../actions/types")
+
+const initialState = []
+
+const Alert = (state = initialState, action) => {
+  // destructure parts of actions
+  const { type, payload } = action
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload]
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload)
+    default:
+      return state
+  }
+}
+
+export default Alert
