e63eb7f49007b05fc8bacf4162cc6a5fde5feb1b
Georgi Danchev <georgicodes@outlook.com>
2022-07-09 09:56:24 +0100

    mern app: front end login and register forms

--- MERN_Stack_Front_To_Back/client/src/components/auth/Login.js
+++ MERN_Stack_Front_To_Back/client/src/components/auth/Login.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+// import axios from "axios"
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  // destructure form data and pull selected values
+  const { email, password } = formData
+
+  // handle input change
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  // handle form submit
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log("user registrations successful")
+
+    // const newUser = {
+    //   name,
+    //   email,
+    //   password,
+    // }
+
+    // try {
+    //   const config = {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+
+    //   const body = JSON.stringify(newUser)
+
+    //   const res = await axios.post("/api/users", body, config)
+
+    //   console.log(res.data)
+    // } catch (error) {
+    //   console.error(error)
+    // }
+  }
+
   return (
-    <div>Login</div>
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" action="create-profile.html" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={(e) => onChange(e)}
+            value={password}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </section>
   )
 }
--- MERN_Stack_Front_To_Back/client/src/components/auth/Register.js
+++ MERN_Stack_Front_To_Back/client/src/components/auth/Register.js
@@ -1,7 +1,110 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+
+// import axios from "axios"
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  })
+
+  // destructure form data and pull selected values
+  const { name, email, password, password2 } = formData
+
+  // handle input change
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  // handle form submit
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      console.log("Passwords do not match")
+    } else {
+      console.log("user registrations successful")
+
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // }
+
+      // try {
+      //   const config = {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+
+      //   const body = JSON.stringify(newUser)
+
+      //   const res = await axios.post("/api/users", body, config)
+
+      //   console.log(res.data)
+      // } catch (error) {
+      //   console.error(error)
+      // }
+    }
+  }
+
   return (
-    <div><h1>Register page!</h1></div>
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" action="create-profile.html" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={(e) => onChange(e)}
+            value={password}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            onChange={(e) => onChange(e)}
+            value={password2}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </section>
   )
 }
