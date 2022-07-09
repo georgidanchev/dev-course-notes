094e4a9788a6484195b586df52c2b41a3c35b903
Georgi Danchev <georgicodes@outlook.com>
2022-07-09 09:13:14 +0100

    marn app: react inital comps and router setup

--- MERN_Stack_Front_To_Back/client/src/App.js
+++ MERN_Stack_Front_To_Back/client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react"
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import { Landing } from "./components/layout/Landing"
+import { Navbar } from "./components/layout/Navbar"
+import { Register } from "./components/auth/Register"
+import { Login } from "./components/auth/Login"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
+)
 
-export default App;
+export default App
