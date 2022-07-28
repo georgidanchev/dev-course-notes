0640057c6c0bc995c4e041942abf95247829ea06
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2022-07-28 15:46:05 +0100

    mern app: add experience and education functionality

--- MERN_Stack_Front_To_Back/client/src/App.js
+++ MERN_Stack_Front_To_Back/client/src/App.js
@@ -1,21 +1,24 @@
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Fragment, useEffect } from "react"
-import { loadUser } from "./actions/auth"
 import Alert from "./components/layout/Alert"
-import ProfileForm from "./components/profile-forms/ProfileForm"
 import Dashboard from "./components/dashboard/Dashboard"
 import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Navbar from "./components/layout/Navbar"
 import PrivateRoute from "./components/routing/PrivateRoute"
+import ProfileForm from "./components/profile-forms/ProfileForm"
 import Register from "./components/auth/Register"
-import setAuthToken from "./utils/setAuthToken"
+import AddExperience from "./components/profile-forms/AddExperience"
+import AddEducation from "./components/profile-forms/AddEducation"
 
 // Redux
 import { Provider } from "react-redux"
 import store from "./store"
+import { loadUser } from "./actions/auth"
+import setAuthToken from "./utils/setAuthToken"
 
+//
 const App = () => {
   // adding [] at the end makes it run only once on mount and unmount
   useEffect(() => {
@@ -38,7 +41,9 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
             <Route path="/create-profile" element={<PrivateRoute component={ProfileForm} />} />
-            <Route path="edit-profile" element={<PrivateRoute component={ProfileForm} />} />
+            <Route path="/edit-profile" element={<PrivateRoute component={ProfileForm} />} />
+            <Route path="/add-experience" element={<PrivateRoute component={AddExperience} />} />
+            <Route path="/add-education" element={<PrivateRoute component={AddEducation} />} />
           </Routes>
         </Fragment>
       </Router>
