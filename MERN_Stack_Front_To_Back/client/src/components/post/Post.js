d8cc556c5ace75b2b3da771a5df63419aa9254e7
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2022-07-30 16:53:19 +0100

    mern-app: comment dispaly and delete func

--- MERN_Stack_Front_To_Back/client/src/components/post/Post.js
+++ MERN_Stack_Front_To_Back/client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { Link, useParams } from "react-router-dom"
 import Spinner from "../layout/Spinner"
 import PostItem from "../posts/PostItem"
 import CommentForm from "./CommentForm"
+import CommentItem from "./CommentItem"
 import { getPost } from "../../actions/post"
 
 const Post = ({ getPost, post: { post, loading } }) => {
@@ -22,7 +23,12 @@ const Post = ({ getPost, post: { post, loading } }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
-      <CommentForm postId={post._id}></CommentForm>
+        <CommentForm postId={post._id}></CommentForm>
+        <div className="comments">
+          {post.comments.map((comment) => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+          ))}
+        </div>
     </section>
   )
 }
