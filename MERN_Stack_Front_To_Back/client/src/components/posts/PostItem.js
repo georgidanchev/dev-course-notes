f01b1d950af9d6a53db9847a666e8b7eda8444ac
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2022-07-30 12:52:14 +0100

    merna app: like and unlike func

--- MERN_Stack_Front_To_Back/client/src/components/posts/PostItem.js
+++ MERN_Stack_Front_To_Back/client/src/components/posts/PostItem.js
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom"
 import Moment from "react-moment"
 import PropTypes from "prop-types"
 import React, { Fragment } from "react"
+import { addLike, removeLike } from "../../actions/post"
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({
+  addLike,
+  removeLike,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+}) => {
   return (
     <Fragment>
       <div className="post bg-white p-1 my-1">
@@ -21,12 +27,12 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
             Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
           </p>
 
-          <button type="button" className="btn btn-light">
+          <button onClick={(e) => addLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-up"></i>{" "}
             {likes.length > 0 && <span className="comment-count">{likes.length}</span>}
           </button>
 
-          <button type="button" className="btn btn-light">
+          <button onClick={(e) => removeLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-down"></i>
           </button>
 
@@ -55,4 +61,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 })
 
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
