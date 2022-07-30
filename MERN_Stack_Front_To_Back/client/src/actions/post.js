b86f4d361b3549542427f7156d32d0a2bf05b6db
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2022-07-30 15:29:37 +0100

    mern-app: delete posts func

--- MERN_Stack_Front_To_Back/client/src/actions/post.js
+++ MERN_Stack_Front_To_Back/client/src/actions/post.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { setAlert } from "./alert"
 
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types"
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types"
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -46,6 +46,25 @@ export const removeLike = (postId) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { postId, likes: res.data },
     })
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Delete post
+export const deletePost = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`)
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId,
+    })
+
+    dispatch(setAlert(res.data.msg, "success"))
   } catch (err) {
     dispatch({
       type: POST_ERROR,
