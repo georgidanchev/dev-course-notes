e2db37cf6316c7089deec0936ca786651055ab92
Georgi Danchev <georgicodes@outlook.com>
2022-09-10 17:38:43 +0100

    fetch error handling notes

---
+++ The_Advanced_Web_Developer_Bootcamp/async-foundations/fetch-error-handling.js
@@ -0,0 +1,22 @@
+// The api URL
+const url = "http://some-url.com/amazing-api"
+
+const apiErrorHandler = (request) => {
+  // Surprise error! We have an error code
+  if (!request.ok) {
+    throw Error(request.status)
+  }
+  // Success! All good bruv!
+  return res
+}
+
+fetch(url)
+  .then(apiErrorHandler)
+  .then((res) => {
+    // We have a response data
+    console.log("it's okay!")
+  })
+  .catch((err) => {
+    // We have an error that has to do with the internet or the request it self and nothing with the response codes.
+    console.error("it's not okay! ERR: " + err)
+  })
