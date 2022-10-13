608dde887672e7c1175a03e875c1188eb1eccc21
Georgi Danchev <georgicodes@outlook.com>
2022-10-13 11:06:52 +0100

    RTCG: copying primitives and references

--- React_The_Complete_Guide/copying.js
+++ React_The_Complete_Guide/copying.js
@@ -2,23 +2,38 @@
 
 let number = 1
 
-const num2 = number;
+const num2 = number
 
 number = 9000
 
 console.log(num2)
 
-
-// Copying reference type: objects, arrays
+// Copying reference type: objects
 
 const person = {
-  name: 'Max'
+  name: "Max",
 }
 
 const secondPerson = {
-  ...person
+  ...person,
 }
 
 person.name = "Manu"
 
-console.log(secondPerson);
+console.log(secondPerson)
+
+// Copying reference type: Arrays
+
+const numbersArray = [1, 2, 3]
+
+const numbersArrayClone = [...numbersArray]
+
+const doubleNumArray = numbersArray.map((num) => {
+  return num * 2
+})
+
+console.log(numbersArray)
+
+console.log(numbersArrayClone)
+
+console.log(doubleNumArray)
