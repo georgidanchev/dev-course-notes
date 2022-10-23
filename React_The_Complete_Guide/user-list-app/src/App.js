7677832c8f3fc45e8a1a1ad538cd86ad225405ef
Georgi Danchev <georgicodes@outlook.com>
2022-10-23 12:46:39 +0100

    RTCG: folders re-organised

--- React_The_Complete_Guide/user-list-app/src/App.js
+++ React_The_Complete_Guide/user-list-app/src/App.js
@@ -1,35 +1,8 @@
-import React, { useState } from "react"
-import UserForm from "./Comps/UserForm"
-import UsersList from "./Comps/UsersList"
-
-const dummy_data = [
-  { name: "madMax100", age: 99 },
-  { name: "Johnny John", age: 88 },
-  { name: "John Doe", age: 88 },
-  { name: "Jane Doe", age: 88 },
-]
+import React from "react"
 
 function App() {
-  const [getUsersList, setUsersList] = useState(dummy_data)
-
-  const onSubmitHandler = (arg1, arg2) => {
-    console.log(arg1, arg2)
-
-    const newUser = {
-      name: arg1,
-      age: arg2,
-    }
-
-    setUsersList((prevUserList) => {
-      return [newUser, ...prevUserList]
-    })
-  }
-
   return (
-    <main>
-      <UserForm onSubmit={onSubmitHandler} />
-      <UsersList usersList={getUsersList} />
-    </main>
+    <div></div>
   )
 }
 
