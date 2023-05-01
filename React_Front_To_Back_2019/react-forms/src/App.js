fb188c52289eac2c8255184d2d74baff4d595ecd
Georgi Danchev <georgicodes@outlook.com>
2023-05-01 19:31:36 +0100

    RTCG: custom email validation hook

--- React_Front_To_Back_2019/react-forms/src/App.js
+++ React_Front_To_Back_2019/react-forms/src/App.js
@@ -1,11 +1,11 @@
-import SimpleInput from './components/SimpleInput';
+import BasicForm from "./components/BasicForm"
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
