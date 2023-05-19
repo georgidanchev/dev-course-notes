76d01d38e9372b195bfedf5abd823408d1325742
Georgi Danchev <georgicodes@outlook.com>
2023-05-19 17:44:42 +0100

    RTCG: redux counter functions

--- React_The_Complete_Guide/redux-basic-counter/src/components/Counter.js
+++ React_The_Complete_Guide/redux-basic-counter/src/components/Counter.js
@@ -1,30 +1,45 @@
-import React, { Component } from "react"
+import React from "react"
 import classes from "./Counter.module.css"
-import { useSelector, useDispatch, connect } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 
 // Functional component
 
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter)
+  const show = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({ type: "increment" })
   }
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 })
+  }
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" })
   }
 
-  const toggleCounterHandler = () => {}
+  const decreaseHandler = () => {
+    dispatch({ type: "decrease", amount: 5 })
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: 'toggle'
+    })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
 
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
 
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
 
