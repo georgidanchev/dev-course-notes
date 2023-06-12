220ce134196311aacff88959cc72d6d5ff255ae3
Georgi Danchev <georgicodes@outlook.com>
2023-06-12 20:26:04 +0100

    RTCG: ...314 - implementing auth

--- React_The_Complete_Guide/react-authentication/frontend/src/pages/Authentication.js
+++ React_The_Complete_Guide/react-authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,52 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom"
+
+import AuthForm from "../components/AuthForm"
 
 function AuthenticationPage() {
-  return <AuthForm />;
+  return <AuthForm />
 }
 
-export default AuthenticationPage;
+export default AuthenticationPage
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams
+
+  const mode = searchParams.get("mode") || "login"
+
+  const data = await request.formData()
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode." }, { status: 422 })
+  }
+
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  }
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  })
+
+  if (response.status === 422 || response.status === 401) {
+    return response
+  }
+
+  if (!response.ok) {
+    throw json(
+      {
+        message: "Could not authenticate user.",
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+
+  // soon: manage the token
+  return redirect("/")
+}
