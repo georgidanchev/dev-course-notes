f608717cbe3b5f7d85b4f63954fe9518dc77ba5f
Georgi Danchev <georgicodes@outlook.com>
2023-06-13 15:26:28 +0100

    RTCG: ...364 - deploying react

--- React_The_Complete_Guide/react-deployments/src/App.js
+++ React_The_Complete_Guide/react-deployments/src/App.js
@@ -1,13 +1,17 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import HomePage from "./pages/Home"
+import RootLayout from "./pages/Root"
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import PostPage, { loader as postLoader } from "./pages/Post"
+
+const BlogPage = lazy(() => import("./pages/Blog"))
+const PostPage = lazy(() => import("./pages/Post"))
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,18 +19,34 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () => import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) => import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
   },
-]);
+])
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
