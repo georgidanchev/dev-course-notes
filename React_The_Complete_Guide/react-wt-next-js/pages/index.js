b376fba32e44b910c1c2d2a2cf5fafc8b671cafe
Georgi Danchev <georgicodes@outlook.com>
2023-06-13 19:49:08 +0100

    RTCG: ...385 - next.js pre-rendering and pre-fetching

--- React_The_Complete_Guide/react-wt-next-js/pages/index.js
+++ React_The_Complete_Guide/react-wt-next-js/pages/index.js
@@ -17,10 +17,18 @@ const DUMMY_MEETUPS = [
   },
 ]
 
-export default function HomePage() {
+export default function HomePage(props) {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={props.meetups} />
     </>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  }
 }
