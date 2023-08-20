02e49940c11d547f4f85b13b5a01c4f9510945d3
Georgi Danchev <georgicodes@outlook.com>
2023-08-20 19:21:21 +0100

    RTCG: ...472 - context api implemented with typescript

--- React_The_Complete_Guide/typescript-wt-react/src/components/Todos.tsx
+++ React_The_Complete_Guide/typescript-wt-react/src/components/Todos.tsx
@@ -1,13 +1,15 @@
-import React from "react"
-import Todo from "../models/todo"
+import React, {useContext} from "react"
 import TodoItem from "./TodoItem"
+import { TodosContext } from "../store/todos-context"
 import classes from "./Todos.module.css"
 
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext)
   return (
+
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
+      {todosCtx.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)} />
       ))}
     </ul>
   )
