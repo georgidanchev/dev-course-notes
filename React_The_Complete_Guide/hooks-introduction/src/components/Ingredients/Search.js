ab8cebb28b782911f54b5bd54610513a20209c0d
Georgi Danchev <georgicodes@outlook.com>
2023-08-26 11:43:41 +0100

    RTCG ...543: react hooks module completed

--- React_The_Complete_Guide/hooks-introduction/src/components/Ingredients/Search.js
+++ React_The_Complete_Guide/hooks-introduction/src/components/Ingredients/Search.js
@@ -1,36 +1,26 @@
 import React, { useState, useEffect, useRef } from "react"
-
 import Card from "../UI/Card"
+import useHttp from "../../hooks/http"
+import ErrorModal from "../UI/ErrorModal"
 import "./Search.css"
 
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props // destructuring object
   const [enteredFilter, setEnteredFilter] = useState("")
   const inputRef = useRef()
+  const { isLoading, data, error, sendRequest, clear } = useHttp()
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query =
-          enteredFilter.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilter}"`
-        fetch(
-          "https://react-hooks-update-35135-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json" +
-            query,
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`
+        sendRequest(
+          `https://react-hooks-update-35135-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json${query}`,
+          "GET",
         )
-          .then((response) => response.json())
-          .then((responseData) => {
-            const loadedIngredients = []
-
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].ingredient.title,
-                amount: responseData[key].ingredient.amount,
-              })
-            }
-
-            onLoadIngredients(loadedIngredients)
-          })
       }
     }, 500)
 
@@ -38,13 +28,32 @@ const Search = React.memo((props) => {
     return () => {
       clearTimeout(timer)
     }
-  }, [enteredFilter, onLoadIngredients, inputRef])
+  }, [enteredFilter, inputRef, sendRequest])
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      console.log(data)
+      const loadedIngredients = []
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount,
+        })
+      }
+
+      onLoadIngredients(loadedIngredients)
+    }
+  }, [data, isLoading, error, onLoadIngredients])
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             ref={inputRef}
             type="text"
