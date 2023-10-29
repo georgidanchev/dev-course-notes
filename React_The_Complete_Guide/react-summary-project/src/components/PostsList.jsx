de9b255ee65e672eb6dcd777dfb78c15cc3de67a
Georgi Danchev <georgicodes@outlook.com>
2023-10-29 14:36:12 +0000

    RTCG: 642 - posts update based on state and fallback text if empty

--- React_The_Complete_Guide/react-summary-project/src/components/PostsList.jsx
+++ React_The_Complete_Guide/react-summary-project/src/components/PostsList.jsx
@@ -19,9 +19,20 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   )
 }
