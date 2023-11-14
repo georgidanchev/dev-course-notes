2e23c3f2c5b5364468b939258946e81139c8e5b8
Georgi Danchev <georgicodes@outlook.com>
2023-11-14 20:30:41 +0000

    RTCG ...115 - styling react components progress

--- React_The_Complete_Guide/basic-react-styling/src/components/Header.jsx
+++ React_The_Complete_Guide/basic-react-styling/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import "./Header.css"
+import classes from "./Header.module.css"
 import logo from "../assets/logo.png"
 
 export default function Header() {
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   )
 }
