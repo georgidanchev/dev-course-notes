c2526d48b6138da8485827f66453bc05fa533a9d
Georgi Danchev <georgicodes@outlook.com>
2023-11-14 13:31:46 +0000

    RTCG: ...107 - initting basic react styles playground project

---
+++ React_The_Complete_Guide/basic-react-styling/src/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from "./components/AuthInputs"
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  )
+}
