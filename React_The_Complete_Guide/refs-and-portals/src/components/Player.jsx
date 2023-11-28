33235a380f4de2061c809713be73032d5a658af6
Georgi Danchev <georgicodes@outlook.com>
2023-11-28 20:47:18 +0000

    RTCG: ...140 - managing user input with two-way-binding

--- React_The_Complete_Guide/refs-and-portals/src/components/Player.jsx
+++ React_The_Complete_Guide/refs-and-portals/src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState, useRef } from "react"
+
 export default function Player() {
+  const playerName = useRef()
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+  const [submitted, setSubmitted] = useState(false)
+
+  function handleChange(event) {
+    setSubmitted(false)
+    setEnteredPlayerName(event.target.value)
+  }
+
+  function handleClick() {
+    setSubmitted(true)
+    setEnteredPlayerName(playerName.current.value)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : " unknown entity"} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick} type="button">
+          Set Name
+        </button>
       </p>
     </section>
-  );
+  )
 }
