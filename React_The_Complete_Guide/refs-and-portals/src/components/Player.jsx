8b7adfac3ac1696da8f7d0bcdcdc4b13c7c4a11c
Georgi Danchev <georgicodes@outlook.com>
2023-12-11 08:28:06 +0000

    small commit

--- React_The_Complete_Guide/refs-and-portals/src/components/Player.jsx
+++ React_The_Complete_Guide/refs-and-portals/src/components/Player.jsx
@@ -3,21 +3,19 @@ import { useState, useRef } from "react"
 export default function Player() {
   const playerName = useRef()
   const [enteredPlayerName, setEnteredPlayerName] = useState(null)
-  const [submitted, setSubmitted] = useState(false)
 
   function handleChange(event) {
-    setSubmitted(false)
     setEnteredPlayerName(event.target.value)
   }
 
   function handleClick() {
-    setSubmitted(true)
     setEnteredPlayerName(playerName.current.value)
+    playerName.current.value = ""
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : " unknown entity"} </h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"} </h2>
       <p>
         <input ref={playerName} type="text" onChange={handleChange} value={enteredPlayerName} />
         <button onClick={handleClick} type="button">
