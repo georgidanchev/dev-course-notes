1e36ff87d105491f3d34f8236adccd4cf3e95c64
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-03-24 14:05:23 +0000

    RTCG: 153 - collect and forward refs

--- React_The_Complete_Guide/state-refs-more/src/components/Input.jsx
+++ React_The_Complete_Guide/state-refs-more/src/components/Input.jsx
@@ -1,12 +1,17 @@
-export default function Input({ label, isTextarea, ...props }) {
-const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+import { forwardRef } from "react"
+
+const Input = forwardRef(function Input({ label, isTextarea, ...props }, ref) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
 
   return (
     <p className="flex flex-col gap-1 my-4">
       <label className="text-sm font-bold uppercase text-stone-500" htmlFor="">
         {label}
       </label>
-      {isTextarea ? <textarea className={classes} {...props} /> : <input className={classes} {...props} />}
+      {isTextarea ? <textarea ref={ref} className={classes} {...props} /> : <input ref={ref} className={classes} {...props} />}
     </p>
   )
-}
+})
+
+export default Input
--- React_The_Complete_Guide/state-refs-more/src/components/NewProject.jsx
+++ React_The_Complete_Guide/state-refs-more/src/components/NewProject.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react"
 import Input from "./Input"
 
-export default function NewPort() {
+export default function NewPort({onAdd}) {
+  const titleRef = useRef()
+  const descriptionRef = useRef()
+  const dueDateRef = useRef()
+
+  function handleSave() {
+    const enteredTitle = titleRef.current.value
+    const enteredDescription = descriptionRef.current.value
+    const enteredDueDate = dueDateRef.current.value
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate
+    })
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,13 +25,14 @@ export default function NewPort() {
           <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
         </li>
       </menu>
+
       <div>
-        <Input label="Title" />
-        <Input label="Description" isTextarea />
-        <Input label="Due Date" />
+        <Input type="text" ref={titleRef} label="Title"  />
+        <Input ref={descriptionRef} label="Description" isTextarea />
+        <Input type="date" ref={dueDateRef} label="Due Date" />
       </div>
     </div>
   )
