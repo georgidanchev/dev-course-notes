a257532a0f33a0b82b1e1a2662f8674abf58d7e7
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-03-24 17:22:16 +0000

    RTCG: 158 - project deletion logic

--- React_The_Complete_Guide/state-refs-more/src/App.jsx
+++ React_The_Complete_Guide/state-refs-more/src/App.jsx
@@ -54,9 +54,19 @@ function App() {
     })
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
   const selectedProject = projectsState.projects.find((project) => projectsState.id === project.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
