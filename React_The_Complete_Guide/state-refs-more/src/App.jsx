32285c4f39edb72a58de83de3dcd667fedbab678
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-03-24 12:05:38 +0000

    RTCG: ...150 - progress something something

--- React_The_Complete_Guide/state-refs-more/src/App.jsx
+++ React_The_Complete_Guide/state-refs-more/src/App.jsx
@@ -1,9 +1,13 @@
+import ProjectsSideBar from "./components/ProjectsSidebar"
+import NewProject from "./components/NewProject"
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar />
+      <NewProject />
+    </main>
+  )
 }
 
-export default App;
+export default App
