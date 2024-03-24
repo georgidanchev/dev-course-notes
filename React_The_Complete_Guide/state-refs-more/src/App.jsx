8092357fe6a56964455ff2e46293f1e83eced052
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-03-24 12:30:40 +0000

    RTCG: 152 - click logic to states switching

--- React_The_Complete_Guide/state-refs-more/src/App.jsx
+++ React_The_Complete_Guide/state-refs-more/src/App.jsx
@@ -1,12 +1,35 @@
 import ProjectsSideBar from "./components/ProjectsSidebar"
 import NewProject from "./components/NewProject"
 import NoProjectSelected from "./components/NoProjectSelected"
+import { useState } from "react"
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  })
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NoProjectSelected />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   )
 }
