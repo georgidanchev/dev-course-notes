c446e68b5b1e1164d9bbb1b2f770070f7eafbddb
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-03-24 12:18:32 +0000

    RTCG: 151 - splitting jsx and tailwind

--- React_The_Complete_Guide/state-refs-more/src/App.jsx
+++ React_The_Complete_Guide/state-refs-more/src/App.jsx
@@ -1,11 +1,12 @@
 import ProjectsSideBar from "./components/ProjectsSidebar"
 import NewProject from "./components/NewProject"
+import NoProjectSelected from "./components/NoProjectSelected"
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   )
 }
