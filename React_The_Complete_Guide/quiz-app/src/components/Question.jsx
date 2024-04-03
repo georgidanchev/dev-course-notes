a495f3b1700175a91d701e96497fb21daf14a82d
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-04-03 12:55:54 +0100

    RTCG: 201 - Setting different timers and fixes

--- React_The_Complete_Guide/quiz-app/src/components/Question.jsx
+++ React_The_Complete_Guide/quiz-app/src/components/Question.jsx
@@ -9,6 +9,16 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
     isCorrect: null,
   })
 
+  let timer = 10000
+
+  if (answer.selectedAnswer) {
+    timer = 1000
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -37,7 +47,12 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[questionIndex].text}</h2>
       <Answers
         answers={QUESTIONS[questionIndex].answers}
