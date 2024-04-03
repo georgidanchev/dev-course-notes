97e897a715f61f82f52f7c10a896c86f875a8117
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-04-03 13:17:00 +0100

    RTCG: 202 - mini quiz app completed

--- React_The_Complete_Guide/quiz-app/src/components/Quiz.jsx
+++ React_The_Complete_Guide/quiz-app/src/components/Quiz.jsx
@@ -1,7 +1,8 @@
 import { useState, useCallback } from "react"
 import QUESTIONS from "../questions"
-import quizCompleteImg from "../assets/quiz-complete.png"
 import Question from "./Question"
+import Summary from "./Summary.jsx"
+
 
 export default function Quiz() {
   const [userAnswers, setUsersAnswers] = useState([])
@@ -20,12 +21,7 @@ export default function Quiz() {
   }, [])
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    )
+    return <Summary userAnswers={userAnswers} />
   }
 
   return (
