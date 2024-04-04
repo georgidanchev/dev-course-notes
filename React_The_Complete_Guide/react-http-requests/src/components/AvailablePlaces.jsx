8f5df5723616d4699cdaab6b481dc2041179adb8
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-04-04 14:48:48 +0100

    RTCG: ...235 - fetching, error handling and sorting data

--- React_The_Complete_Guide/react-http-requests/src/components/AvailablePlaces.jsx
+++ React_The_Complete_Guide/react-http-requests/src/components/AvailablePlaces.jsx
@@ -1,12 +1,58 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react"
+import Places from "./Places.jsx"
+import Error from "./Error"
+import { sortPlacesByDistance } from "../loc"
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false)
+  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [errorText, setErrorText] = useState()
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true)
+
+      try {
+        const response = await fetch("http://localhost:3000/places")
+        const resData = await response.json()
+
+        if (!response.ok) {
+          // 200 300
+          throw new Error("Failed to fetch places")
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          )
+          setAvailablePlaces(sortedPlaces)
+          setIsFetching(false)
+        })
+      } catch (error) {
+        setErrorText({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        })
+      }
+    }
+
+    fetchPlaces()
+  }, [])
+
+  if (errorText) {
+    return <Error title="An error occurred!" message={errorText.message} />
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
     />
-  );
+  )
 }
