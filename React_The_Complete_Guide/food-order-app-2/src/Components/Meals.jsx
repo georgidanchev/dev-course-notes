a2e5dee491bad1e23598a2572e66b347a70aa4c2
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-04-14 20:33:39 +0100

    RTCG: 279 - HTTP loading and error states

--- React_The_Complete_Guide/food-order-app-2/src/Components/Meals.jsx
+++ React_The_Complete_Guide/food-order-app-2/src/Components/Meals.jsx
@@ -1,13 +1,19 @@
 // import { useEffect, useState } from "react"
 import MealItem from "./MealItem"
 import useHttp from "../hooks/useHttp"
+import Error from "./Error"
+
 const requestConfig = {}
 
 export default function Meals() {
   const { data: loadedMeals, isLoading, error } = useHttp("http://localhost:3000/meals", requestConfig, [])
 
   if (isLoading) {
-    return <p>Fetching meals!</p>
+    return <p className="center">Fetching meals!</p>
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error}></Error>
   }
 
   return (
